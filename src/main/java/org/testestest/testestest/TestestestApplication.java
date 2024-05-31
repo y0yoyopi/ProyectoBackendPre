6c5cc06a944e4b2ea05fd47ecc1cece9b333ff9c
Y0yoyopi <you@example.com>
2024-05-30 21:55:55 -0500

    Reseting Basis

---
+++ src/main/java/org/testestest/testestest/TestestestApplication.java
@@ -0,0 +1,13 @@
+package org.testestest.testestest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestestestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestestestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/testestest/testestest/TestestestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.testestest.testestest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestestestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
